6f586edd11cd64df16c5c740770c2227b466e008
v <v@v.(none)>
2011-11-19 00:40:09 +0000

    first commit

---
+++ retrieve_value.pl
@@ -0,0 +1,31 @@
+#!/usr/bin/perl -w 
+use LWP::Simple;
+use WWW::Mechanize;
+use HTML::TreeBuilder;
+
+#secure login for three.ie , Ireland. Works when dongle is plugged in, in Ubuntu.
+my $url = 'https://my3.three.ie/';
+
+my $mech = WWW::Mechanize->new();
+$mech->agent_alias( 'Linux Mozilla' );
+$mech->get( $url );
+
+# follow first link(logged in now)
+$mech->follow_link( n => 1 );
+
+# view account information
+$mech->submit_form(
+	form_number => 1
+);
+
+my $tree = HTML::TreeBuilder->new_from_content($mech->content);
+
+# extract the remaining Mbs left. Stored within tds
+foreach my $td ($tree->look_down(_tag => 'td')) {
+	$in_cell = $td->as_text;
+
+	# only get the actual value, discard everything else
+	if($in_cell =~ m/,/) {  
+	    print $in_cell . "\n";
+	}
+}
